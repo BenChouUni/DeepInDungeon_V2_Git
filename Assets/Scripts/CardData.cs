891c6b4b2373a6f06298ed8a86ab0de9e4bba4fb
BenChouUni <ben900202chou@gmail.com>
2022-12-07 21:40:21 +0800

    CardSO
    更新卡牌SO

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardData
+{
+    public int id;
+    public string cardName;
+    public int cost;
+
+    public CardData()
+    {
+        this.id = 0;
+        this.cardName = "";
+        this.cost = 0;
+    }
+    public CardData(string _cardName) : this()
+    {
+        this.cardName = _cardName;
+    }
+
+}
+
+[CreateAssetMenu(fileName = "NewCardSO" , menuName = "Create SO/Card SO")]
+public class CardSO : ScriptableObject
+{
+    public CardData cardData;
+}
