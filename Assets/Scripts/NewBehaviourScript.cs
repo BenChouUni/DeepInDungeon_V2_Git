7477c74527706f6ba7db13467c00eb2725ce1673
kou-ming <t0937993806@gmail.com>
2022-12-15 22:45:56 +0800

    InitialCardManager_UPDATE
    InitialCardManager更新，可成功生成

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public List<CardSO> InicialCardList;
+    public GameObject InicialCardPrefab;
+    public Transform InicialCardListPanel;
+
+
+    private void Start()
+    {
+        CreateInicialCardOnPanel(1);
+    }
+    public void CreateInicialCardOnPanel(int id)
+    {
+        CardData inicialcardData = GetInicialCardData(id);
+        GameObject new_inicialcard;
+
+        if (inicialcardData != null)
+        {
+            new_inicialcard = Instantiate(InicialCardPrefab, InicialCardListPanel, false);
+            new_inicialcard.GetComponent<CardDisplay>().CardData = inicialcardData;
+        }
+    }
+
+    public CardData GetInicialCardData(int id)
+    {
+        foreach (CardSO item in InicialCardList)
+        {
+            if (item.cardData.id == id)
+            {
+                return item.cardData;
+            }
+        }
+        return null;
+    }
+}
