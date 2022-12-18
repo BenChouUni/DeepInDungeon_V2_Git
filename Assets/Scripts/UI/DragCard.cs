31bb5f13b01f9fd503f459844994d602b8cfb4a2
BenChouUni <ben900202chou@gmail.com>
2022-12-18 23:57:48 +0800

    DragCard
    實現卡牌拖動

---
+++ Assets/Scripts/UI/DragCard.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//用event system
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class DragCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    //初始parent 以便放開後返回
+
+    private Transform StartParent;
+    private Transform parentReturnTo = null;//要返回的位置的parent
+
+    private GameObject canvas;
+
+
+    private void Start()
+    {
+        canvas = GameObject.Find("Canvas").gameObject;
+
+    }
+
+
+
+    public void OnBeginDrag(PointerEventData eventData)//開始拖動執行一次
+    {
+
+        parentReturnTo = this.transform.parent;
+
+
+        //拖動時移到canvas底下
+        this.transform.SetParent(canvas.transform);
+
+        //關閉Raycast Block
+        TurnRaycastBlock(false);
+    }
+
+
+    public void OnDrag(PointerEventData eventData)//拖動中
+    {
+        this.transform.position = eventData.position;
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        this.transform.SetParent(parentReturnTo);
+
+        //重新打開raycast的影響
+        TurnRaycastBlock(true);
+    }
+
+    public void ReturnToStartParent()
+    {
+        this.transform.SetParent(StartParent);
+    }
+
+    private void TurnRaycastBlock(bool value)
+    {
+        this.GetComponent<CanvasGroup>().blocksRaycasts = value;
+    }
+}
+
