23f42fa16a3abe9e1b313971780db84985c88ee6
BenChouUni <ben900202chou@gmail.com>
2023-01-19 17:59:53 +0800

    WeaponDataBaseEditor
    完成出版WeaponDataBase Editor

--- Assets/Editor/WeaponDataBaseEditor.cs
+++ Assets/Editor/WeaponDataBaseEditor.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.IO;
+using UnityEditor.VersionControl;
 
 [CustomEditor(typeof(WeaponDataBase))]
 public class WeaponDataBaseEditor : Editor
@@ -14,6 +16,10 @@ public class WeaponDataBaseEditor : Editor
 
         foreach (WeaponSO item in weaponDataBase.weaponDataList)
         {
+            if (item == null)
+            {
+                continue;
+            }
             list.Add(item.weaponData);
         }
 
@@ -21,25 +27,100 @@ public class WeaponDataBaseEditor : Editor
 
         for (int i = 0; i < list.Count; i++)
         {
-            EditorGUILayout.Space();
-            EditorGUILayout.BeginHorizontal();
+            CreateWeaponDataRow(list, i);
+        }
 
-            EditorGUILayout.LabelField("ID", GUILayout.MaxWidth(20));
-            list[i].id = EditorGUILayout.IntField(list[i].id);
+        EditorGUILayout.Space();
 
-            EditorGUILayout.LabelField("Name", GUILayout.MaxWidth(45));
-            list[i].weaponName = EditorGUILayout.TextField(list[i].weaponName);
+        if (GUILayout.Button("Add New WeaponSO"))
+        {
+            WeaponSO so = CreateWeaponSOAsset();
+            weaponDataBase.weaponDataList.Add(so);
+            Repaint();
+        }
 
-            EditorGUILayout.LabelField("ATK", GUILayout.MaxWidth(30));
-            list[i].atk = EditorGUILayout.IntField(list[i].atk);
+        if (GUILayout.Button("Save Name"))
+        {
+            foreach (WeaponSO item in weaponDataBase.weaponDataList)
+            {
+                SaveName(item);
+            }
 
-            EditorGUILayout.LabelField("DEF", GUILayout.MaxWidth(30));
-            list[i].def = EditorGUILayout.IntField(list[i].def);
-
-            EditorGUILayout.LabelField("Max Distance", GUILayout.MaxWidth(70));
-            list[i].distance = EditorGUILayout.IntField(list[i].distance);
-
-            EditorGUILayout.EndHorizontal();
         }
     }
+
+    private static void SaveName(WeaponSO item)
+    {
+        string assetPath = AssetDatabase.GetAssetPath(item.GetInstanceID());
+        WeaponData weaponData = item.weaponData;
+        WeaponData NewData = new WeaponData(weaponData.id, weaponData.weaponName, weaponData.atk, weaponData.def, weaponData.distance);
+
+        Debug.Log(NewData.weaponName);
+        AssetDatabase.RenameAsset(assetPath, item.weaponData.weaponName);
+        Debug.Log(NewData.weaponName);
+        //會創建一個新的檔案，舊的會找不到，所以要重新給值
+
+
+
+        assetPath = AssetDatabase.GetAssetPath(item.GetInstanceID());
+        WeaponSO newSO = AssetDatabase.LoadAssetAtPath<WeaponSO>(assetPath);
+        newSO.weaponData = NewData;
+
+        AssetDatabase.SaveAssets();
+    }
+
+    private static void CreateWeaponDataRow(List<WeaponData> list, int i)
+    {
+        if (list[i] == null)
+        {
+            return;
+        }
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+
+        EditorGUILayout.LabelField("ID", GUILayout.MaxWidth(20));
+        list[i].id = EditorGUILayout.IntField(list[i].id);
+
+        EditorGUILayout.LabelField("Name", GUILayout.MaxWidth(45));
+        list[i].weaponName = EditorGUILayout.TextField(list[i].weaponName);
+
+        EditorGUILayout.LabelField("ATK", GUILayout.MaxWidth(30));
+        list[i].atk = EditorGUILayout.IntField(list[i].atk);
+
+        EditorGUILayout.LabelField("DEF", GUILayout.MaxWidth(30));
+        list[i].def = EditorGUILayout.IntField(list[i].def);
+
+        EditorGUILayout.LabelField("Max Distance", GUILayout.MaxWidth(70));
+        list[i].distance = EditorGUILayout.IntField(list[i].distance);
+
+        EditorGUILayout.EndHorizontal();
+    }
+
+
+
+    [MenuItem("Asset/Create/WeaponSO")]
+    public static WeaponSO CreateWeaponSOAsset()
+    {
+        WeaponSO asset = ScriptableObject.CreateInstance<WeaponSO>();
+        /*
+        if (!asset)
+        {
+            Debug.LogWarning("WeaponSO not found");
+            return;
+        }*/
+
+        string path = "Assets/SO/WeaponSO/NewWeaponSO.asset";
+        if (!Directory.Exists(path))
+        {
+            Directory.CreateDirectory(path);
+        }
+        AssetDatabase.CreateAsset(asset, path);
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        //Selection.activeObject = asset;
+        return asset;
+    }
+
 }
