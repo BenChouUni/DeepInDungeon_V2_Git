5fb9444c22909d84e7ad95fa37cfbdea91c816dc
BenChouUni <ben900202chou@gmail.com>
2023-01-31 03:45:37 +0800

    1/31update
    更新一些缺失的東西

---
+++ Assets/Scripts/BattleManagers/TrunPhaseManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GamePhase
+{
+    GameStart,PlayerDraw,PlayerAction,EnemyAction,GameEnd
+}
+
+public class TrunPhaseManager : MonoSingleton<TrunPhaseManager>
+{
+
+}
