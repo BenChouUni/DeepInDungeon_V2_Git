8949ad5443ef53dd35f6000a758a3784b5f7211b
BenChouUni <ben900202chou@gmail.com>
2023-02-13 01:44:50 +0800

    update
    小更新

--- Assets/Scripts/Manager/BattleManagers/ActiobManager.cs
+++ Assets/Scripts/Manager/BattleManagers/ActiobManager.cs
@@ -6,7 +6,16 @@ public class ActiobManager : MonoBehaviour
 {
     public void UseAction(Action action,Character character)
     {
-
+        int id = action.id;
+        int parameter = action.parameter;
+        switch (id)
+        {
+            case 0:
+                AttackAction(character, parameter);
+                break;
+            default:
+                break;
+        }
     }
 
     private void AttackAction(Character character,int damage)
--- Assets/Scripts/Manager/BattleManagers/EnemyManager.cs
+++ Assets/Scripts/Manager/BattleManagers/EnemyManager.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyManager : MonoBehaviour
 {
     public EnemyData enemyData;
+    //UIShow
+    public Text enemyName;
+    public HealthBar enemyHealthBar;
 
+    private void Awake()
+    {
+        //暫時使用
+        enemyData = new EnemyData(0, "木樁", 50, 0, 3);
+    }
     public void ShowEnemy()
     {
-
+        enemyName.text = enemyData.Name;
+        enemyHealthBar.SetMaxHealth(enemyData.MaxHp);
+        enemyHealthBar.SetHealth(enemyData.CurrentHp);
     }
 
 }
