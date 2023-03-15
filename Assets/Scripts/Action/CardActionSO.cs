687f7703020057d3da26dc589010b63b3f124aa5
BenChouUni <ben900202chou@gmail.com>
2023-03-16 03:32:13 +0800

    ActionManagerFix
    改回武器攻擊防禦可以附加數值
    創建異常狀態SO

---
+++ Assets/Scripts/Action/CardActionSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardActionSO : ScriptableObject,ICardAction
+{
+    public string actionName;
+    
+
+    public void Act(CardData card, Character actor, Character target)
+    {
+        
+    }
+
+    
+
+}
