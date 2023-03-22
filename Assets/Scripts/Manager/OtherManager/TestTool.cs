3cd71718365cc6a8fcfa563d6c1df907a55370d6
kou-ming <t0937993806@gmail.com>
2023-03-22 23:11:02 +0800

    make_AwardSystem
    新增戰鬥勝利後可選擇獎勵

---
+++ Assets/Scripts/Manager/OtherManager/TestTool.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTool : MonoSingleton<TestTool>
+{
+    public void Click()
+    {
+        BattleMainManager.instance.enemyManager.enemyData.isDeath = true;
+        Debug.Log("¼Ä¤H¦º¤`");
+    }
+}
