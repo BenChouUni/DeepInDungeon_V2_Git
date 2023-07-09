cf2c6a611808d62a3c1404bd4ca4b213279dc69a
kou-ming <t0937993806@gmail.com>
2023-07-09 21:15:15 +0800

    update_titlescene
    更新titlescene

--- Assets/Scripts/Manager/OtherManager/TitleMainManager.cs
+++ Assets/Scripts/Manager/OtherManager/TitleMainManager.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleMainManager : MonoBehaviour
 {
-    public void Click()
+    public GameObject LoadGame_Button;
+    private void Start()
+    {
+        //確認有沒有存檔
+        if (!DataPersistenceManager.instance.Check_Has_Date())
+        {
+            LoadGame_Button.GetComponent<Button>().interactable = false;
+            UnityEngine.Object.DestroyImmediate(LoadGame_Button.GetComponent<ZoomInUI>());
+        }
+        else
+        {
+            LoadGame_Button.GetComponent<Button>().interactable = true;
+            LoadGame_Button.GetComponent<ZoomInUI>().ZoomSize = 1.2f;
+        }
+    }
+
+    public void New()
     {
         DataPersistenceManager.instance.NewGame();
-
-        //data.playerData.setCharaterName(nameInput);
         DataPersistenceManager.instance.SaveGame();
+        SceneManager.LoadScene(5);
+    }
+
+    public void Load()
+    {
         SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("退出遊戲");
+        Application.Quit();
     }
 }
