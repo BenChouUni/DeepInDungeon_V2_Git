324960e2c39a805007fdc8512c310b5d04671df4
BenChouUni <ben900202chou@gmail.com>
2023-07-14 05:25:24 +0800

    EnemyGiveState

---
+++ Assets/Scripts/EnemyAction/AllEnemyActions/EnemyGiveEffect.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGiveEffect : EnemyActionBase
+{
+    public override EnemyActionType type => EnemyActionType.GiveEffect;
+
+    public override void DoAction(EnemyActionParameter parameter, EnemyData enemyData)
+    {
+        Debug.LogFormat("敵人施加{0}{1}層", parameter.stateEffectType,parameter.value);
+
+        Character targetCharater = parameter.Target;
+        StateEffect stateEffect = parameter.StateEffect;
+        stateEffect.setLayer(parameter.value);
+        targetCharater.AddStateEffect(stateEffect);
+
+    }
+}
